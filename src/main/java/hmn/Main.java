87a3c60c56c72009b98cab6a6e0a12800173705f
jwke21 <jakevm21@gmail.com>
2023-09-12 09:23:31 -0700

    Updated main

--- src/main/java/hmn/Main.java
+++ src/main/java/hmn/Main.java
@@ -1,12 +1,15 @@
 package hmn;
 
 import hmn.graphicsLib.Window;
+import hmn.music.MusicEd;
 import hmn.sandbox.Paint;
 
 public class Main {
 
   public static void main(String[] args) {
-    Window.PANEL = new Paint();
-    Window.launch();
+//    Window.PANEL = new Paint();
+//    Window.launch();
+    Window.PANEL = new MusicEd();
+    Window.PANEL.launch();
   }
 }
